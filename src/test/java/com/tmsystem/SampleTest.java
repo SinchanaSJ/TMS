37d6ee003bd7eb03eb004dc879737f44290e3e1c
Admin <Admin@Lenovo>
2023-09-27 16:05:25 +0530

    2nd Commit

---
+++ src/test/java/com/tmsystem/SampleTest.java
@@ -0,0 +1,11 @@
+package com.tmsystem;
+
+import org.testng.annotations.Test;
+
+public class SampleTest {
+@Test
+public void sampleTest() {
+	System.out.println("--- Sample Test 1---");
+	System.out.println("--- Sample Test 2---");
+}
+}
