704e9cd485e46e013ed1ab4e73ed83b7a71b04b5
Admin <Admin@Lenovo>
2023-09-27 12:57:01 +0530

    1st commit

---
+++ src/main/java/com/TMS/genericUtils/RetryImpl.java
@@ -0,0 +1,17 @@
+package com.TMS.genericUtils;
+
+import org.testng.IRetryAnalyzer;
+import org.testng.ITestResult;
+
+public class RetryImpl implements IRetryAnalyzer{
+	int count=0;
+	int upperLmt=3;
+	public boolean retry(ITestResult result) {
+		if(count<upperLmt) {
+			count++;
+			return true;
+		}
+		return false;
+	}
+
+}
